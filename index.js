0f335859d9c90fbb1bc5350f2815935a4e4505d0
Gautam <gautamsharda001@gmail.com>
2021-07-20 16:27:16 -0500

    make it better

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+const Note = require('./models/note')
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
 
 let notes = [
     {
@@ -33,19 +38,15 @@ let notes = [
   })
 
   app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
       response.json(notes)
+    })  
   })
 
   app.get('/api/notes/:id', (request, response) => {
-      const id = Number(request.params.id)
-      const note = notes.find(note => note.id === id)
-      if (note){
-          response.json(note)
-      }
-      else{
-          response.status(404).end()
-      }
-
+    Note.findById(request.params.id).then(note => {
+      response.json(note)
+    })
   })
 
   app.delete('/api/notes/:id', (request, response) => {
@@ -64,18 +65,23 @@ let notes = [
   }
   
   app.post('/api/notes', (request, response) => {
+    const body = request.body
   
-    if (!request.body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
   
-    notes = notes.concat(request.body)
-    console.log('hiiiii')
-    response.json(request.body)
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+    })
+  
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
   })
-
+  
   app.put('/api/notes/:id', (request, response) => {
     
     for (i = 0; i < notes.length; i++){
